cf931696dc5f9081c47ef5e48393185771edb879
Yordan Stoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-19 08:14:02 +0200

    Add files via upload

---
+++ Problem1.NationalCourt/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Problem1.NationalCourt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstEmployeeEfficiency = int.Parse(Console.ReadLine());
+            int secondEmployeeEfficiency = int.Parse(Console.ReadLine());
+            int thirdEmployeeEfficiency = int.Parse(Console.ReadLine());
+
+            int peopleCount = int.Parse(Console.ReadLine());//broi vaprosi
+
+            int totalEmployeeEfficiency = firstEmployeeEfficiency
+                + secondEmployeeEfficiency
+                + thirdEmployeeEfficiency;//broi otgovori na chas
+            // vseki chetvarti chas e pochivka
+            int hour = 0;
+            
+            while (peopleCount > 0)
+            {
+                peopleCount -= totalEmployeeEfficiency;
+                hour++;
+                if (hour % 4 == 0)
+                {
+                    hour++;
+                }
+            }
+            Console.WriteLine("Time needed: {0}h.", Math.Floor((double)hour));//broyat chasove se zakruglya kum po golyamoto cyalo chislo
+            
+        }
+    }
+}
