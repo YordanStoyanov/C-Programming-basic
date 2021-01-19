f1049d7a1911431032c30dcd6cfd5581d9db5477
Yordan Stoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-19 08:14:44 +0200

    Add files via upload

---
+++ NextLevel/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextLevel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int neededExperience = int.Parse(Console.ReadLine());
+            int countOfBattles = int.Parse(Console.ReadLine());
+            int experienceEarnedPerBattle = 0;
+            int sum = 0;
+            int totalNeededExperience = neededExperience;
+            for (int i = 0; i < countOfBattles; i++)
+            {
+                if (sum > totalNeededExperience)
+                {
+                    Console.WriteLine("Player successfully collected his needed experience for {0} battles.", i);
+                    break;
+                }
+
+                if (neededExperience < 0)
+                {
+                    Console.WriteLine("Player was not able to collect the needed experience, {0:f2} more needed.", Math.Abs(neededExperience));
+                    break;
+                }
+
+                sum += experienceEarnedPerBattle;
+                if (i % 3 == 0)
+                {
+                    experienceEarnedPerBattle *= 15;
+                    experienceEarnedPerBattle /= 100;
+                    sum += experienceEarnedPerBattle;
+
+                }
+                else if (i % 5 == 0)
+                {
+                    experienceEarnedPerBattle *= 10;
+                    experienceEarnedPerBattle /= 100;
+                    sum -= experienceEarnedPerBattle;
+                }
+                else if (i % 15 == 0)
+                {
+                    experienceEarnedPerBattle *= 5;
+                    experienceEarnedPerBattle /= 100;
+                    sum += experienceEarnedPerBattle;
+                }
+                neededExperience -= sum;
+                experienceEarnedPerBattle = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
