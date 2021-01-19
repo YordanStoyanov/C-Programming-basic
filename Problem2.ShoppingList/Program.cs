e14bd224990bfb5323aa74f78b899f69a8093d08
Yordan Stoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-19 08:13:35 +0200

    Add files via upload

---
+++ Problem2.ShoppingList/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Linq;
+
+namespace Problem2.ShoppingList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split('!');
+            List
+        }
+    }
+}
