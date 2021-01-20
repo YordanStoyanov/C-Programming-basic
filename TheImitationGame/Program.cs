34f490f4cfaf908df3267f8cf88f10504e750396
Yordan Stoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-20 12:25:34 +0200

    Add files via upload

---
+++ TheImitationGame/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheImitationGame
+{
+    class Program
+    {
+        public static object StringBuilred { get; private set; }
+
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string message = Console.ReadLine();
+            while (message != "Decode")
+            {
+                StringBuilder moveElements = new StringBuilder();
+                if (message.Contains("Move"))
+                {
+                    string[] parts = message.Split("|");
+                    int moveNumberOfElements = int.Parse(parts[1]);
+                    string newWord = input;
+                    moveElements.Append(input);
+                    moveElements.Remove(0, moveNumberOfElements);
+                }
+                message = Console.ReadLine();
+            }
+        }
+    }
+}
+/*
+zzHe
+Move|3
+Decode
+
+ */
