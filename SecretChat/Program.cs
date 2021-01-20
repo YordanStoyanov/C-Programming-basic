fefee201c471d2a9a59928ab4d69837241b4f304
Yordan Stoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-20 12:25:18 +0200

    Add files via upload

---
+++ SecretChat/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace SecretChat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Console.ReadLine();
+            string commands = Console.ReadLine();
+
+            while (commands != "Reveal")
+            {
+                if (commands.Contains("ChangeAll"))
+                {
+                    string[] parts = commands.Split(":|:");
+                    string substring = parts[1];
+                    string replacement = parts[2];
+                    message = message.Replace(substring, replacement);
+                    Console.WriteLine(message);
+                }
+
+                if (commands.Contains("Reverse"))
+                {
+                    string[] parts = commands.Split(":|:");
+                    string substring = parts[1];
+
+                }
+            }
+        }
+    }
+}
