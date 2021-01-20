5eb195bec775d36ed34c9b79383a9ca843cbfa4c
Yordan Stoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-20 12:23:53 +0200

    Add files via upload

---
+++ FundamentalsExam/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundamentalsExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            s = Console.ReadLine();
+            foreach (char c in s)
+            {
+                Console.WriteLine((int)c);
+            }
+        }
+    }
+}
